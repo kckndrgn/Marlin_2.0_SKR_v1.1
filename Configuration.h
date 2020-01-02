2a7f7b44cc1696ce01e42dfdb03fb00b813f6ccf
kckndrgn <ryanm@rmcmullen.org>
2020-01-01 21:36:53 -0600

    updated configuration
    Updated the Serial definition, machine name and UUID

--- Configuration.h
+++ Configuration.h
@@ -103,7 +103,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 0
+#define SERIAL_PORT -1
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
@@ -135,11 +135,11 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SKR 1.3"
+#define CUSTOM_MACHINE_NAME "SKR 1.1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "a1dc9266-2d10-11ea-978f-2e728ce88125"
 
 // @section extruder
 
